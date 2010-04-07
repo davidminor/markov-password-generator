d0ed3da15fd82d088f099727100ce032d44f6929
David Minor <daveminor@gmail.com>
2010-04-07 11:02:50 -0700

    added probability storage

--- generate_password.rb
+++ generate_password.rb
@@ -1,6 +1,9 @@
 # Generate english-like words using markov chains (or other language depending on dictionary)
 DICTIONARY_FILE = '/usr/share/dict/words'
 
+# Set this to a file name to store the probabilities for better repeat performance
+PROBABILITY_FILE = nil # '~/.generate_password_cache'
+
 def usage
   puts "ruby generate_password.rb WORD_LENGTH [NUM_TO_GENERATE]"
   exit
@@ -12,39 +15,43 @@ usage if ( word_length < 3 || word_count < 1 )
 
 base = "a"[0]
 
-# Compute probabilities of 3 letter sequences (could be computed once and stored for reading)
-probs = Array.new(27) { |i| Array.new(27) { |j| Array.new(27) { |k| 0 }}}
-pen_probs = Array.new(27) { |i| Array.new(27) { |j| Array.new(27) { |k| 0 }}}
-last_probs = Array.new(27) { |i| Array.new(27) { |j| Array.new(27) { |k| 0 }}}
+probs, pen_probs, last_probs = nil, nil, nil
 
-# Count each sequence of 3 specific letters
-File.open( DICTIONARY_FILE ) do |file|
-  file.each_line do |line|
-    next if line.length < 5
-    next if line !~ /^[a-zA-Z]+$/
-    prev = [ 26, 26 ] # 26 indicates no letter
-    prob_arr = [probs]*(line.chomp!.length - 2) + [pen_probs, last_probs]
-    line.downcase.each_byte do |c|
-      c -= base
-      prob_arr.shift[prev[-2]][prev[-1]][c] += 1
-      prev << c
+if (!PROBABILITY_FILE || !File.exist?(PROBABILITY_FILE) )
+  
+  # Compute probabilities of 3 letter sequences
+  make_arr = lambda { Array.new(27) { |i| Array.new(27) { |j| Array.new(27) { |k| 0 }}} }
+  probs, pen_probs, last_probs = make_arr[], make_arr[], make_arr[]
+
+  # Count each sequence of 3 specific letters
+  File.open( DICTIONARY_FILE ) do |file|
+    MATCHING_LINE = /^[a-zA-Z]{4,}$/
+    file.each_line do |line|
+      next if line !~ MATCHING_LINE
+      prev = [ 26, 26 ] # 26 indicates no letter
+      prob_arrs = [probs]*(line.chomp!.length - 2) + [pen_probs, last_probs]
+      line.downcase.each_byte { |c| prob_arr.shift[prev[-2]][prev[-1]][(prev << (c-base))[-1]] += 1 }
     end
   end
-end
 
-# normalize
-[ probs, pen_probs, last_probs ].each do |prob_arr|
-  prob_arr.each do |second_letter|
-    second_letter.each do |third_letter|
-      sum = third_letter.inject(0) { |s,count| s += count }
-      third_letter.map! { |count| count.to_f()/sum } if sum > 0
+  # normalize
+  prob_arrs = [ probs, pen_probs, last_probs ]
+  prob_arrs.each do |prob_arr|
+    prob_arr.each do |second_letter|
+      second_letter.each do |third_letter|
+        sum = third_letter.inject(0) { |s,count| s += count }
+        third_letter.map! { |count| count.to_f()/sum } if sum > 0
+      end
     end
   end
+  
+  Marshal.dump(prob_arrs, File.new(PROBABILITY_FILE, "wb")) if (PROBABILITY_FILE)
+  
+else
+  probs, pen_probs, last_probs = *Marshal.load(File.open(PROBABILITY_FILE, "rb"))
 end
 
-srand( Time.now.to_i )
 word_count.times do
-
   chars = []
   ([probs]*(word_length-2) + [pen_probs, last_probs]).each do |prob_arr|
     total = 0
